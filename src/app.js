e0027691a28aa8cff414cb53ce176d2434ac1093
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2015-08-30 23:11:22 -0600

    Blaaaaah

--- src/app.js
+++ src/app.js
@@ -1,4 +1,32 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+        posts = require('./mocks/posts.json'),
+        app = express();
+
+
+/**
+* Set our template engine and correct path
+*/
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/blog/:title?', function (req, res) {
+  var title = req.params.title;
+  if (title === undefined) {
+    res.status(503);
+    res.send('This page is under construction');
+  } else {
+    var post = posts[title] || {};
+    res.render('post', {post: post});
+  }
+});
+
+app.listen(3000, function() {
+  console.log('Party on port 3000');
+});
 
